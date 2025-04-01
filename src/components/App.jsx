b96b4214da4dfe807dd304ac835e6792aa5e6d8f
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-04-01 09:38:38 +0300

    GoIT React Home Work №1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,17 @@
-import './App.css';
+import userData from '../data/userData.json';
+import Profile from './Profile/Profile';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+export default App;
